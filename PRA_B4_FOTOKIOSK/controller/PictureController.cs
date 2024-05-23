92fc1d930faaa907e8c3e83c4eafeee209d72b5b
GwnDani <daanspeelgames@gmail.com>
2024-05-23 11:23:58 +0200

    A1 - Display van dagen geregeld
    Als je nu het programma opstart zie je alleen de foto's van die dag.

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -11,17 +11,18 @@ namespace PRA_B4_FOTOKIOSK.controller
 {
     public class PictureController
     {
-        // De window die we laten zien op het scherm
+        // De window die we laten zien op het scherm 
         public static Home Window { get; set; }
-
 
         // De lijst met fotos die we laten zien
         public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
-        
-        
+
         // Start methode die wordt aangeroepen wanneer de foto pagina opent.
         public void Start()
         {
+            // Verkrijg het huidige dagnummer (0 = Zondag, 6 = Zaterdag)
+            var now = DateTime.Now;
+            int day = (int)now.DayOfWeek;
 
             // Initializeer de lijst met fotos
             // WAARSCHUWING. ZONDER FILTER LAADT DIT ALLES!
@@ -32,13 +33,19 @@ namespace PRA_B4_FOTOKIOSK.controller
                  * dir string is de map waar de fotos in staan. Bijvoorbeeld:
                  * \fotos\0_Zondag
                  */
-                foreach (string file in Directory.GetFiles(dir))
+
+                // Haal de naam van de map op
+                var dirName = Path.GetFileName(dir);
+                if (dirName.StartsWith(day.ToString() + "_"))
                 {
-                    /**
-                     * file string is de file van de foto. Bijvoorbeeld:
-                     * \fotos\0_Zondag\10_05_30_id8824.jpg
-                     */
-                    PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                    foreach (string file in Directory.GetFiles(dir))
+                    {
+                        /**
+                         * file string is het bestand van de foto. Bijvoorbeeld:
+                         * \fotos\0_Zondag\10_05_30_id8824.jpg
+                         */
+                        PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                    }
                 }
             }
 
@@ -49,7 +56,7 @@ namespace PRA_B4_FOTOKIOSK.controller
         // Wordt uitgevoerd wanneer er op de Refresh knop is geklikt
         public void RefreshButtonClick()
         {
-
+            // Hier kan je de logica toevoegen voor wat er moet gebeuren wanneer de refresh knop wordt ingedrukt
         }
 
     }
