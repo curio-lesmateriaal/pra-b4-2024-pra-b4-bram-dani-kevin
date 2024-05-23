c412ae14aba6226cbaaf6ce236e2c590923a162f
GwnDani <daanspeelgames@gmail.com>
2024-05-23 11:29:59 +0200

    A2 - Nu displayed hij alleen maar foto's die tussen de 2 en 30 minuten geleden zijn genomen.
    - Aangepaste Start-methode om een filter toe te voegen voor foto's die tussen 2 en 30 minuten geleden zijn genomen.
    - Tijdinformatie uit de bestandsnamen van de foto's gehaald.
    - Het tijdsverschil tussen de huidige tijd en de fototijd berekend.
    - Foto's alleen aan de weergavelijst toegevoegd als ze aan de tijdcriteria voldoen.
    
    Dit zorgt ervoor dat bezoekers alleen recente foto's zien die relevant zijn voor hun bezoek.

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -14,7 +14,7 @@ namespace PRA_B4_FOTOKIOSK.controller
         // De window die we laten zien op het scherm 
         public static Home Window { get; set; }
 
-        // De lijst met fotos die we laten zien
+        // De lijst met foto's die we laten zien
         public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
 
         // Start methode die wordt aangeroepen wanneer de foto pagina opent.
@@ -24,13 +24,13 @@ namespace PRA_B4_FOTOKIOSK.controller
             var now = DateTime.Now;
             int day = (int)now.DayOfWeek;
 
-            // Initializeer de lijst met fotos
+            // Initializeer de lijst met foto's
             // WAARSCHUWING. ZONDER FILTER LAADT DIT ALLES!
             // foreach is een for-loop die door een array loopt
             foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
             {
                 /**
-                 * dir string is de map waar de fotos in staan. Bijvoorbeeld:
+                 * dir string is de map waar de foto's in staan. Bijvoorbeeld:
                  * \fotos\0_Zondag
                  */
 
@@ -44,12 +44,28 @@ namespace PRA_B4_FOTOKIOSK.controller
                          * file string is het bestand van de foto. Bijvoorbeeld:
                          * \fotos\0_Zondag\10_05_30_id8824.jpg
                          */
-                        PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                        // Haal de tijd uit de bestandsnaam
+                        var fileName = Path.GetFileNameWithoutExtension(file);
+                        var timeParts = fileName.Split('_');
+                        if (timeParts.Length >= 3)
+                        {
+                            int hour = int.Parse(timeParts[0]);
+                            int minute = int.Parse(timeParts[1]);
+                            int second = int.Parse(timeParts[2]);
+
+                            var fileTime = new DateTime(now.Year, now.Month, now.Day, hour, minute, second);
+                            var timeDifference = now - fileTime;
+
+                            if (timeDifference.TotalMinutes >= 2 && timeDifference.TotalMinutes <= 30)
+                            {
+                                PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                            }
+                        }
                     }
                 }
             }
 
-            // Update de fotos
+            // Update de foto's
             PictureManager.UpdatePictures(PicturesToDisplay);
         }
 
@@ -58,6 +74,5 @@ namespace PRA_B4_FOTOKIOSK.controller
         {
             // Hier kan je de logica toevoegen voor wat er moet gebeuren wanneer de refresh knop wordt ingedrukt
         }
-
     }
 }
