13cb90379c16b0715814da2927c73713daf32fd6
GwnDani <daanspeelgames@gmail.com>
2024-06-05 11:40:22 +0400

    Django heeft geholpen
    Django heeft geholpen ik heb de hele rebase van de picturecontroller aangepast

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -11,68 +11,72 @@ namespace PRA_B4_FOTOKIOSK.controller
 {
     public class PictureController
     {
-        // De window die we laten zien op het scherm 
+        // De window die we laten zien op het scherm
         public static Home Window { get; set; }
 
-        // De lijst met foto's die we laten zien
+        // De lijst met fotos die we laten zien
         public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
+        public List<string> PhotoInList = new List<string>();
 
         // Start methode die wordt aangeroepen wanneer de foto pagina opent.
         public void Start()
         {
-            // Verkrijg het huidige dagnummer (0 = Zondag, 6 = Zaterdag)
             var now = DateTime.Now;
             int day = (int)now.DayOfWeek;
 
-            // Initializeer de lijst met foto's
-            // WAARSCHUWING. ZONDER FILTER LAADT DIT ALLES!
-            // foreach is een for-loop die door een array loopt
-            foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
+            // Zet de dir op goede plek
+            string dir = "";
+            if (day == 0) dir = "../../../fotos/0_Zondag";
+            else if (day == 1) dir = "../../../fotos/1_Maandag";
+            else if (day == 2) dir = "../../../fotos/2_Dinsdag";
+            else if (day == 3) dir = "../../../fotos/3_Woensdag";
+            else if (day == 4) dir = "../../../fotos/4_Donderdag";
+            else if (day == 5) dir = "../../../fotos/5_Vrijdag";
+            else if (day == 6) dir = "../../../fotos/6_Zaterdag";
+
+            // haalt photo op
+            foreach (string file in Directory.GetFiles(dir))
             {
-                /**
-                 * dir string is de map waar de foto's in staan. Bijvoorbeeld:
-                 * \fotos\0_Zondag
-                 */
+                int hour = now.Hour;
+                int minute = now.Minute - 2;
+                int second = now.Second;
 
-                // Haal de naam van de map op
-                var dirName = Path.GetFileName(dir);
-                if (dirName.StartsWith(day.ToString() + "_"))
+                for (int i = 0; i < 1800; i++)
                 {
-                    foreach (string file in Directory.GetFiles(dir))
+                    second--;
+                    if (second < 0)
                     {
-                        /**
-                         * file string is het bestand van de foto. Bijvoorbeeld:
-                         * \fotos\0_Zondag\10_05_30_id8824.jpg
-                         */
-                        // Haal de tijd uit de bestandsnaam
-                        var fileName = Path.GetFileNameWithoutExtension(file);
-                        var timeParts = fileName.Split('_');
-                        if (timeParts.Length >= 3)
-                        {
-                            int hour = int.Parse(timeParts[0]);
-                            int minute = int.Parse(timeParts[1]);
-                            int second = int.Parse(timeParts[2]);
+                        second = 59;
+                        minute--;
+                    }
 
-                            var fileTime = new DateTime(now.Year, now.Month, now.Day, hour, minute, second);
-                            var timeDifference = now - fileTime;
+                    if (minute < 0)
+                    {
+                        minute = 59;
+                        hour--;
+                    }
 
-                            if (timeDifference.TotalMinutes >= 2 && timeDifference.TotalMinutes <= 30)
-                            {
-                                PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
-                            }
-                        }
+                    string timeDateString = string.Format("{0:D2}_{1:D2}_{2:D2}_", hour, minute, second);
+
+                    // Check if the file name contains the timeDateString
+                    if (Path.GetFileName(file).Contains(timeDateString))
+                    {
+                        // Add the photo to the PicturesToDisplay list
+                        PicturesToDisplay.Add(new KioskPhoto() { Id = PicturesToDisplay.Count, Source = file });
+                        // Add the time string to the PhotoInList
+                        PhotoInList.Add(timeDateString);
                     }
                 }
             }
 
-            // Update de foto's
+            // Update de fotos
             PictureManager.UpdatePictures(PicturesToDisplay);
         }
 
         // Wordt uitgevoerd wanneer er op de Refresh knop is geklikt
         public void RefreshButtonClick()
         {
-            // Hier kan je de logica toevoegen voor wat er moet gebeuren wanneer de refresh knop wordt ingedrukt
+
         }
     }
 }
