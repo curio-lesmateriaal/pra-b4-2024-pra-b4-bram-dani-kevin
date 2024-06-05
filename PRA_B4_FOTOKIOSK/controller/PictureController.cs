c38b7e636c5cf8e193f7ce115862a4db5995f83f
GwnDani <daanspeelgames@gmail.com>
2024-06-05 12:11:00 +0400

    PRA A3
    Ervoor gezorg dat de foto's naast elkaar komen te staan die bij elkaar horen

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -4,17 +4,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PRA_B4_FOTOKIOSK.controller
 {
     public class PictureController
     {
-        // De window die we laten zien op het scherm
+        // Het venster dat we op het scherm laten zien
         public static Home Window { get; set; }
 
-        // De lijst met fotos die we laten zien
+        // De lijst met foto's die we laten zien
         public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
         public List<string> PhotoInList = new List<string>();
 
@@ -24,7 +22,7 @@ namespace PRA_B4_FOTOKIOSK.controller
             var now = DateTime.Now;
             int day = (int)now.DayOfWeek;
 
-            // Zet de dir op goede plek
+            // Zet de map op de juiste plek
             string dir = "";
             if (day == 0) dir = "../../../fotos/0_Zondag";
             else if (day == 1) dir = "../../../fotos/1_Maandag";
@@ -34,49 +32,69 @@ namespace PRA_B4_FOTOKIOSK.controller
             else if (day == 5) dir = "../../../fotos/5_Vrijdag";
             else if (day == 6) dir = "../../../fotos/6_Zaterdag";
 
-            // haalt photo op
+            // Lijst om alle foto's met hun tijdstempels te bewaren
+            List<Tuple<DateTime, string>> allPhotos = new List<Tuple<DateTime, string>>();
+
+            // Lees foto's en parse tijdstempels
             foreach (string file in Directory.GetFiles(dir))
             {
-                int hour = now.Hour;
-                int minute = now.Minute - 2;
-                int second = now.Second;
+                string fileName = Path.GetFileName(file);
+                DateTime timestamp = ParseTimestampFromFileName(fileName);
 
-                for (int i = 0; i < 1800; i++)
+                if (timestamp != DateTime.MinValue)
                 {
-                    second--;
-                    if (second < 0)
-                    {
-                        second = 59;
-                        minute--;
-                    }
-
-                    if (minute < 0)
-                    {
-                        minute = 59;
-                        hour--;
-                    }
-
-                    string timeDateString = string.Format("{0:D2}_{1:D2}_{2:D2}_", hour, minute, second);
-
-                    // Check if the file name contains the timeDateString
-                    if (Path.GetFileName(file).Contains(timeDateString))
-                    {
-                        // Add the photo to the PicturesToDisplay list
-                        PicturesToDisplay.Add(new KioskPhoto() { Id = PicturesToDisplay.Count, Source = file });
-                        // Add the time string to the PhotoInList
-                        PhotoInList.Add(timeDateString);
-                    }
+                    allPhotos.Add(new Tuple<DateTime, string>(timestamp, file));
                 }
             }
 
-            // Update de fotos
+
+            // Koppel foto's die 60 seconden uit elkaar zijn genomen
+            for (int i = 0; i < allPhotos.Count - 1; i++)
+            {
+                DateTime currentTimestamp = allPhotos[i].Item1;
+                string currentPhoto = allPhotos[i].Item2;
+
+                if ((allPhotos[i + 1].Item1 - currentTimestamp).TotalSeconds == 60)
+                {
+                    string pairedPhoto = allPhotos[i + 1].Item2;
+
+                    PicturesToDisplay.Add(new KioskPhoto() { Id = PicturesToDisplay.Count, Source = currentPhoto });
+                    PicturesToDisplay.Add(new KioskPhoto() { Id = PicturesToDisplay.Count, Source = pairedPhoto });
+
+                    PhotoInList.Add(currentTimestamp.ToString("HH_mm_ss"));
+                    PhotoInList.Add(allPhotos[i + 1].Item1.ToString("HH_mm_ss"));
+
+                    i++; // Sla de volgende foto over omdat deze al is gekoppeld
+                }
+            }
+
+            // Update de foto's
             PictureManager.UpdatePictures(PicturesToDisplay);
+        }
+
+        private DateTime ParseTimestampFromFileName(string fileName)
+        {
+            // Neem aan dat het bestandsnaamformaat "HH_mm_ss_<other_info>.jpg" is
+            try
+            {
+                string[] parts = fileName.Split('_');
+                int hour = int.Parse(parts[0]);
+                int minute = int.Parse(parts[1]);
+                int second = int.Parse(parts[2]);
+
+                return new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, hour, minute, second);
+            }
+            catch
+            {
+                // Verwerk parse fouten of onverwachte bestandsnaamformaten
+                return DateTime.MinValue;
+            }
         }
 
         // Wordt uitgevoerd wanneer er op de Refresh knop is geklikt
         public void RefreshButtonClick()
         {
-
+            // Logica voor de refresh knop
         }
     }
 }
